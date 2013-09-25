48f59d2ca0ad478c94220343edae877e1ccae1d3
Xiaoyu He <alkjash876@gmail.com>
2013-09-25 18:36:28 -0400

    Removed two levels of useless refactoring, still no idea how to make it not recurse indefinitely

--- ps1/combparser.ml
+++ ps1/combparser.ml
@@ -21,14 +21,12 @@ let ls ((p1 : ('c, 'a) parser), (p2 : ('c, 'b) parser)) : ('c, 'a * 'b) parser =
 (* Constructs an expression parser *)
 (* Expressions factor as:
    aexp = Int | Var | ( exp )
-   bexp = aexp | aexp / bexp
-   cexp = bexp | bexp * cexp
-   dexp = cexp | cexp - dexp
-   eexp = dexp | dexp + eexp
-   fexp = eexp | !fexp
-   gexp = fexp | fexp (== , != , < , <= , > , >=) gexp
-   hexp = gexp | gexp && hexp | gexp || hexp
-   exp = hexp | Var = exp
+   bexp = aexp | aexp / bexp | aexp * bexp
+   cexp = bexp | bexp - cexp | bexp + cexp
+   dexp = cexp | !dexp
+   eexp = dexp | dexp (== , != , < , <= , > , >=) eexp
+   fexp = eexp | eexp && fexp | eexp || fexp
+   exp = fexp | Var = exp
 
    Extra levels of refactoring were added to make arithmetic bind completely correctly.
    *)
@@ -52,44 +50,36 @@ let rec make_aexp_parser (() : unit) : (token, exp) parser =
   let paren_exp_parser = map (fun (_, (e, _)) -> e) paren_parser in
   alts [int_exp_parser; var_exp_parser; paren_exp_parser]
 
-(* bexp = aexp | aexp / bexp *)
+(* bexp = aexp | aexp / bexp | aexp * bexp *)
 and make_bexp_parser (() : unit) : (token, exp) parser = 
   let slash_parser = ls (make_aexp_parser (), 
 	ls (tok SLASH, make_bexp_parser ())) in
   let div_exp_parser = map (fun (e1, (_, e2)) -> (Binop(e1, Div, e2), dummy_pos)) slash_parser in
-  alt (make_aexp_parser (), div_exp_parser)
-
-(* cexp = bexp | bexp * cexp *)
-and make_cexp_parser (() : unit) : (token, exp) parser = 
-  let star_parser = ls (make_bexp_parser (), 
-	ls (tok STAR, make_cexp_parser ())) in
+  let star_parser = ls (make_aexp_parser (), 
+	ls (tok STAR, make_bexp_parser ())) in
   let times_exp_parser = map (fun (e1, (_, e2)) -> (Binop(e1, Times, e2), dummy_pos)) star_parser in
-  alt (make_bexp_parser (), times_exp_parser)
+  alts [make_aexp_parser (); div_exp_parser; times_exp_parser]
 
-(* dexp = cexp | cexp - dexp *)
-and make_dexp_parser (() : unit) : (token, exp) parser = 
-  let minus_parser = ls (make_cexp_parser (), 
-	ls (tok MINUS, make_dexp_parser ())) in
+(* cexp = bexp | bexp - cexp | bexp + cexp *)
+and make_cexp_parser (() : unit) : (token, exp) parser = 
+  let minus_parser = ls (make_bexp_parser (), 
+	ls (tok MINUS, make_cexp_parser ())) in
   let minus_exp_parser = map (fun (e1, (_, e2)) -> (Binop(e1, Minus, e2), dummy_pos)) minus_parser in
-  alt (make_cexp_parser (), minus_exp_parser)
-
-(* eexp = dexp | dexp + eexp *)
-and make_eexp_parser (() : unit) : (token, exp) parser = 
-  let plus_parser = ls (make_dexp_parser (), 
-	ls (tok PLUS, make_eexp_parser ())) in
+  let plus_parser = ls (make_bexp_parser (), 
+	ls (tok PLUS, make_cexp_parser ())) in
   let plus_exp_parser = map (fun (e1, (_, e2)) -> (Binop(e1, Plus, e2), dummy_pos)) plus_parser in
-  alt (make_dexp_parser (), plus_exp_parser)
+  alts [make_bexp_parser (); minus_exp_parser; plus_exp_parser]
 
-(* fexp = eexp | !fexp *)
-and make_fexp_parser (() : unit) : (token, exp) parser = 
-  let not_parser = ls (tok NOT, make_fexp_parser ()) in
+(* dexp = cexp | !dexp *)
+and make_dexp_parser (() : unit) : (token, exp) parser = 
+  let not_parser = ls (tok NOT, make_dexp_parser ()) in
   let not_exp_parser = map (fun (_, e) -> (Not(e), dummy_pos)) not_parser in
-  alt (make_eexp_parser (), not_exp_parser)
+  alt (make_cexp_parser (), not_exp_parser)
 
-(* gexp = fexp | fexp (== , != , < , <= , > , >=) gexp *)
-and make_gexp_parser (() : unit) : (token, exp) parser = 
-  let compare_parser = ls (make_fexp_parser (), 
-	ls (alts [tok EQ; tok NEQ; tok LT; tok LTE; tok GT; tok GTE], make_gexp_parser ())) in
+(* eexp = dexp | dexp (== , != , < , <= , > , >=) eexp *)
+and make_eexp_parser (() : unit) : (token, exp) parser = 
+  let compare_parser = ls (make_dexp_parser (), 
+	ls (alts [tok EQ; tok NEQ; tok LT; tok LTE; tok GT; tok GTE], make_eexp_parser ())) in
   let compare_exp_parser = map (fun (e1, (op, e2)) -> match op with
       EQ -> (Binop(e1, Eq, e2), dummy_pos)
     | NEQ -> (Binop(e1, Neq, e2), dummy_pos)
@@ -98,19 +88,19 @@ and make_gexp_parser (() : unit) : (token, exp) parser =
     | GT -> (Binop(e1, Gt, e2), dummy_pos)
     | GTE -> (Binop(e1, Gte, e2), dummy_pos)
     | _ -> raise FatalError) compare_parser in
-  alt (make_fexp_parser (), compare_exp_parser)
+  alt (make_dexp_parser (), compare_exp_parser)
 
-(* hexp = gexp | gexp && hexp | gexp || hexp *)
-and make_hexp_parser (() : unit) : (token, exp) parser = 
-  let andor_parser = ls (make_gexp_parser (), 
-	ls (alt (tok AND, tok OR), make_hexp_parser ())) in
+(* fexp = eexp | eexp && fexp | eexp || fexp *)
+and make_fexp_parser (() : unit) : (token, exp) parser = 
+  let andor_parser = ls (make_eexp_parser (), 
+	ls (alt (tok AND, tok OR), make_fexp_parser ())) in
   let andor_exp_parser = map (fun (e1, (op, e2)) -> match op with
       AND -> (And(e1, e2), dummy_pos)
     | OR -> (Or(e1, e2), dummy_pos)
     | _ -> raise FatalError) andor_parser in
-  alt (make_gexp_parser (), andor_exp_parser)
+  alt (make_eexp_parser (), andor_exp_parser)
 
-(* exp = hexp | Var = exp *)
+(* exp = fexp | Var = exp *)
 and make_exp_parser (() : unit) : (token, exp) parser = 
   let assign_parser = ls (satisfy (fun t -> match t with
 	  VAR(_) -> true
@@ -119,7 +109,7 @@ and make_exp_parser (() : unit) : (token, exp) parser =
   let assign_exp_parser = map (fun h -> match h with
 	  (VAR(v), (_, e)) -> (Assign(v, e), dummy_pos)
 	| _ -> raise FatalError) assign_parser in
-  alt (make_hexp_parser (), assign_exp_parser)
+  alt (make_fexp_parser (), assign_exp_parser)
 
 (* Parses a statement of the form Exp, If-Else, While, For, Return, { stmt } 
    Does not match the empty list to guarantee that make_stmt_parser doesn't go into
