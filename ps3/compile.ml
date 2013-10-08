5b0c59d0b961d6e363ea8e796678ec70a6134a45
Xiaoyu He <alkjash876@gmail.com>
2013-10-07 21:54:46 -0400

    Wrote dummy code breaking up work

--- ps3/compile.ml
+++ ps3/compile.ml
@@ -1,5 +1,6 @@
 (* Compile Cish AST to MIPS AST *)
 open Mips
+open Ast
 
 exception IMPLEMENT_ME
 
@@ -11,10 +12,50 @@ let label_counter = ref 0
 let new_int() = (label_counter := (!label_counter) + 1; !label_counter)
 let new_label() = "L" ^ (string_of_int (new_int()))
 
-let rec compile (p:Ast.program) : result =
+(************ Environment and variable map declarations ****************)
+
+(* varmap: immutable association list of vars and offsets *)
+type varmap = (Ast.var * int) list
+(* TODO:
+   Define empty_varmap(), insert_var(), lookup_var() *)
+
+type env = {varmap : varmap}
+
+(************ Helper functions *****************************************)
+(* make_env(): create environment for a function, passing through the code once:
+   Association list (var * int) which keeps track of where temporary variables are
+   with respect to frame pointer. int is the number of words from the frame pointer
+   where the base of var starts. *)
+let rec make_env (f : Ast.func) : env =
+	raise IMPLEMENT_ME
+
+(* push and pop: wrappers for pushing and popping temps off of the stack *)
+(* push writes assembly code down that pushes the value of register r onto the current
+   sp and updating sp *)
+let push (r : Mips.reg) : Mips.inst list =
+	raise IMPLEMENT_ME
+
+(* pop writes assembly code down that pops off the top of sp into register r and decrements
+   sp *)
+let pop (r : Mips.reg) : Mips.inst list =
+	raise IMPLEMENT_ME
+
+(* Set up stack frame for new procedure; calls make_env to set up local variables first *)
+let frame_start (f : Ast.func) : Mips.inst list =
+	raise IMPLEMENT_ME
+
+(* Actually compile a function: first calls frame_start to set up stack frame, then compiles
+   body of procedure, then makes epilogue for return *)
+let compile_func (f : Ast.func) : Mips.inst list =
+	raise IMPLEMENT_ME
+
+(* TODO: More subfunctions needed: frame_end, frame_save *)
+
+(************* Compile *************************************************)
+let rec compile (p : Ast.program) : result =
     raise IMPLEMENT_ME
 
-let result2string (res:result) : string = 
+let result2string (res : result) : string = 
     let code = res.code in
     let data = res.data in
     let strs = List.map (fun x -> (Mips.inst2string x) ^ "\n") code in
