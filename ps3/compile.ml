2f77ad65f32dce2fd703874a2ef43a161adf2ff6
Xiaoyu He <alkjash876@gmail.com>
2013-10-14 14:27:45 -0400

    Finished code

--- ps3/compile.ml
+++ ps3/compile.ml
@@ -23,50 +23,39 @@ type varmap = (Ast.var * int) list
 
 let empty_varmap = [] 
 
-let vm_insert (vm : varmap) (v : Ast.var) (offset : int) : varmap =
-	("MOO" ^ v, offset) :: vm 
+(* Insert a new 4-word variable which has offset equal to 4 more than the last one *)
+let vm_insert (vm : varmap) (v : Ast.var) : varmap =
+	let offset = match vm with
+	  h::t -> (snd h) + 4
+	| [] -> 4 in
+	(v, offset) :: vm 
 
 let vm_delete (vm : varmap) : varmap =
 	List.tl vm
 
-(*
-let vm_newscope (vm : varmap) : varmap =
-	("BRACE", 0) :: vm
-
-let rec vm_endscope (vm : varmap) : varmap = 
-	match vm with
-	  ("BRACE", 0) :: t -> t
-	| _ :: t -> vm_endscope t
-	| [] -> []
-*)
-
 (* Returns -1 if not found, otherwise positive offset *)
 let rec vm_lookup (vm : varmap) (v : Ast.var) : int =
-	let var = "MOO" ^ v in
 	match vm with
 	  [] -> -1
-	| h :: t -> (fun (v2, o) -> if v2 = var then o else
-		lookup_var t v) h
-
-(* Creates a new temporary variable and cons it to the head of varmap *)
-let new_temp (vm : varmap) : varmap = 
-	let v = "T" ^ (string_of_int (new_int())) in
-	v :: vm
+	| h :: t -> (fun (v2, o) -> if v2 = v then o else
+		vm_lookup t v) h
 
 type env = {varmap : varmap; epilogue : Mips.label}
 
 (************ Helpers **************************************************)
 
+let zero = Word32.fromInt 0
+
 (* push and pop: wrappers for pushing and popping vars on and off of the stack *)
 (* push writes assembly code down that pushes the value of register r onto the current
    sp and updating sp *)
 let push (r : Mips.reg) : Mips.inst list =
-	[Sw(r,R29,zero); Sub(R29,R29,4)]
+	[Add(R29,R29, Immed(Word32.fromInt (-4))); Sw(r, R29, zero)]
 
 (* pop writes assembly code down that pops off the top of sp into register r and decrements
    sp *)
 let pop (r : Mips.reg) : Mips.inst list =
-	[Lw(r,R29,zero); Add(R29,R29,4)]
+	[Lw(r, R29, zero); Add(R29, R29, Immed(Word32.fromInt 4))]
 
 (************ Statement-Level Compilation ******************************)
 (* Essentially the same as Fish statements except treat variables differently
@@ -74,17 +63,13 @@ let pop (r : Mips.reg) : Mips.inst list =
    Every function now takes and returns an env to keep varmap updated, and know
    where to return to (epilogue) in case of Return statement *)
 
-let zero = Word32.fromInt 0
-
-let rec compile_explist (lst : exp list) (en :env) : inst list =
-  match lst with
-    h::t -> (compile_exp h en) @ (push R3) @ (compile_explist t en)
-  | _ -> []
-
 (* The only guarantee we make of an expression is that R3 is stored with its value after it is 
    finished evaluating. It may or may not clobber R4 in the process.
    Compile_exp doesn't handle scope changes or Lets, so we don't have to worry about
-   passing back a modified varmap *)
+   passing back a modified varmap 
+   Because our code is written in such a way that any expression is free to clobber any
+   registers except R3, we don't need to worry about storing caller registers before
+   performing a function call *)
 let rec compile_exp ((e , _) : Ast.exp) (en : env) : inst list =
 	let vm = en.varmap in
     match e with
@@ -106,7 +91,7 @@ let rec compile_exp ((e , _) : Ast.exp) (en : env) : inst list =
     	| Div -> [Mips.Div(R3, R4, R3)]
     	| Eq -> [Mips.Seq(R3, R4, R3)]
     	| Neq -> [Sne(R3, R4, R3)]
-    	| Lt -> [Slt(R3, R4, R3)]
+    	| Lt -> [Slt(R3, R4, Reg R3)]
     	| Lte -> [Sle(R3, R4, R3)]
     	| Gt -> [Sgt(R3, R4, R3)]
     	| Gte -> [Sge(R3, R4, R3)]
@@ -114,6 +99,7 @@ let rec compile_exp ((e , _) : Ast.exp) (en : env) : inst list =
 
     | Not(e) -> 
 	(compile_exp e en) @ [Mips.Seq(R3, R3, R0)] (* Set R3 to 1 if zero, zero otherwise *)
+
     | And(e1, e2) -> 
     let l = new_label() in
     (* If e1 = 0 jump directly to end, otherwise push it to stack *)
@@ -139,9 +125,21 @@ let rec compile_exp ((e , _) : Ast.exp) (en : env) : inst list =
 	(compile_exp e en) @ [Add(R4, R30, Immed(Word32.fromInt (-offset))); Sw(R3, R4, zero)] 
 
 	| Call (f, arglist) ->
-  (compile_explist arglist en) @ [Jal(f)]
-  
-
+	(* Pushes the arguments for a function to the stack before calling it; moves stack pointer back
+   	to before these arguments so that the function knows that the nargs words after the current
+   	stack pointer are its arguments;
+   	Technically, we should be putting the first four into registers but we make this simplification
+   	which can be thought of
+   	as just having the caller save them onto the stack for the callee instead of letting the
+   	callee save them, as it would probably do anyways. *)
+	(* Compile each argument expression individually and push value onto stack *)
+	let ilistlist = List.map (fun e -> (compile_exp e en) @ (push R3)) arglist in
+	(* Concatenate expression code all together *)
+	let ilist = List.fold_right (fun a b -> a @ b) ilistlist [] in
+	(* Shift sp to before arguments *)
+	ilist @ [Add(R29, R29, Immed(Word32.fromInt (4 * (List.length arglist)))); 
+	(* After return store value of R2 (f's return value) into R3 *)
+	Jal("FUNC" ^ f); Add(R3, R2, Immed(zero))]
 
 let rec compile_stmt ((s,_):Ast.stmt) (en : env) : (inst list * env) = 
     match s with
@@ -172,11 +170,15 @@ let rec compile_stmt ((s,_):Ast.stmt) (en : env) : (inst list * env) =
     | For(e1,e2,e3,s) ->
         compile_stmt (Seq((Exp e1, 0),(While(e2,(Seq(s,(Exp e3,0)),0)), 0)), 0) en
 	| Let(v, e, s) ->
-		(compile_exp e en) @ [La(R4, v); Sw(R3, R4, zero)] @ (compile_stmt s en)
-    (* Store the result of R3 into R2 for return, then jump to epilogue *)
-    | Return(e) -> (((compile_exp e en) @ [Mips.Add(R2, R3, Reg(R0)); Jr(epilogue)]), en) 
-
-
+		(* Create a new sub-environment en2 with extra variable to execute s in *)
+		((compile_exp e en) @ (push R3) @
+		(let vm2 = vm_insert en.varmap v in 
+		let en2 = {varmap = vm2; epilogue = en.epilogue} in 
+		fst (compile_stmt s en2)) @ (pop R3), en) (* Store the value computed for v into R3 *)
+    (* Store the result of R3 into R2 for return, then jump to epilogue;
+	   We store it in R16 as well solely for debugging purposes *)
+    | Return(e) -> (((compile_exp e en) @ [Mips.Add(R2, R3, Reg(R0)); 
+			Mips.Add(R16, R3, Reg(R0)); J(en.epilogue)]), en) 
 
 (************ Procedure-Level Compilation ******************************)
 (* Stack Frame setup for prologue:
@@ -189,181 +191,65 @@ let rec compile_stmt ((s,_):Ast.stmt) (en : env) : (inst list * env) =
 	Return address
 	--- Stack pointer ---
 
-	If n < 4 then there are at least 4 anyways. The first four arg spaces are left empty
-	and it's up to the callee to store them there, which we always do for consistency.
+	If n < 4 then there are at least 4 anyways. In the MIPS calling convention, the first 
+	four arg spaces are left empty and it's up to the callee to store them there, 
+	but for simplicity we have the caller do that for them.
 *)
 
-(* make_env(): create environment for a function, passing through the code once:
-   Association list (var * int) which keeps track of where temporary variables are
-   with respect to frame pointer. int is the number of words from the frame pointer
-   where the base of var starts. *)
-let rec args_list (args : var list) : env =
-  | h::t -> (h,R29-R30) @ make_env t
-  | _ -> []
+(* make_env(): create environment for a function, just adds the arguments to the varmap
+   and creates an epilogue label *)
+let make_env (fsig : Ast.funcsig) : env =
+	let rec add_args alist =
+		match alist with
+		  [] -> empty_varmap
+		| h::t -> 
+			let vm = add_args t in
+			vm_insert vm h in
+	let ep = new_label() in
+	{varmap = add_args fsig.args; epilogue = ep}
 
-let rec make_env (fsig : Ast.funcsig) : env =
-	args_list fsig.args
-
-(* Set up stack frame for new procedure;  *)
-let frame_start (fsig : Ast.funcsig) : Mips.inst list =
+(* Prologue *)
+let frame_start (fsig : Ast.funcsig) (e : env) : (Mips.inst list * env) =
 	let nargs = List.length fsig.args in
-	(push R30) @ 
+	let vm = e.varmap in
+	let vm = vm_insert vm "CALLER_FRAME_POINTER" in
+	let vm = vm_insert vm "RETURN_ADDRESS" in
+	(* Save old frame pointer R30 into stack, make old stack pointer new frame pointer, shift
+       stack pointer down the size of nargs (and two more by the pushes) words *)
+	let ilist = 
+		[Add(R3, R30, Reg R0); Add(R30, R29, Reg R0); Li(R4, Word32.fromInt(nargs * 4));
+		  Sub(R29, R29, R4)] in
+	(ilist @ (push R3) @ (push R31), {varmap = vm; epilogue = e.epilogue}) 
 
-(* Clean up before return *)
-let frame_end (fsig : Ast.funcsig) : Mips.inst list =
-	let nargs = List.length fsig.args in
-	(push R30) @ 
+(* Epilogue *)
+let frame_end (fsig : Ast.funcsig) (e : env) : Mips.inst list =
+	let caller_fp = vm_lookup e.varmap "CALLER_FRAME_POINTER" in
+	let ra = vm_lookup e.varmap "RETURN_ADDRESS" in
+	(* Load correct return address *)
+	[Add(R3, R30, Immed(Word32.fromInt (-ra))); Lw(R31, R3, zero);
+	(* Move sp up to fp *)	
+	Add(R29, R30, Reg R0);
+	(* Move fp up to old fp and return *)
+	Add(R3, R30, Immed(Word32.fromInt (-caller_fp))); Lw(R30, R3, zero); Jr R31]
 
 (* Actually compile a function: first calls frame_start to set up stack frame, then compiles
    body of procedure, then makes epilogue for return *)
 let compile_func (f : Ast.func) : Mips.inst list =
 	let Fn(fsig) = f in
 	let env = make_env fsig in
-	let head = Label ("FUNC" ^ fsig.name) :: (frame_start fsig) in 
+	let (ilist, env) = frame_start fsig env in
+	let name = if fsig.name = "main" then "main" else "FUNC" ^ fsig.name in
+	let prologue = (Label name) :: ilist in 
+	let (body, _) = compile_stmt fsig.body env in
+	let ilist2 = frame_end fsig env in
+	let epilogue = (Label env.epilogue) :: ilist2 in
+	prologue @ body @ epilogue
 	
-
-(* TODO: More subfunctions needed: frame_end, frame_save *)
-
 (************* Compile *************************************************)
-(*
-(* sets of variables -- Ocaml Set and Set.S *)
-module VarSet = Set.Make(struct
-                           type t = Ast.var
-                           let compare = String.compare
-                         end)
-
-(* a table of variables that we need for the code segment *)
-let variables : VarSet.t ref = ref (VarSet.empty)
-
-(* generate a fresh temporary variable and store it in the variables set. *)
-let rec new_temp() = 
-    let t = "T" ^ (string_of_int (new_int())) in
-    (* make sure we don't already have a variable with the same name! *)
-    if VarSet.mem t (!variables) then new_temp()
-    else (variables := VarSet.add t (!variables); t)
-
-
-(* Helper to add a variable to the variables Set; Prepends "MOO" to variable names
- * to avoid conflicts with MIPs codes *)
-let var_add (s : string) : unit =
-  ignore (variables := VarSet.add ("MOO" ^ s) (!variables))
-
-(* Helper to find all the variables in an expression and add
- * them to the set variables *)
-let rec collect_vars_exp (e : Ast.exp) : unit =
-  match fst e with
-    Int(_) -> ()
-  | Var(x) -> var_add x
-  | Binop(e1, _, e2) -> (collect_vars_exp e1; collect_vars_exp e2)
-    | Not(e) -> collect_vars_exp e
-  | And(e1, e2) -> (collect_vars_exp e1; collect_vars_exp e2)
-  | Or(e1, e2) -> (collect_vars_exp e1; collect_vars_exp e2)
-  | Assign(x, e) -> (var_add x; collect_vars_exp e) 
-
-(* find all of the variables in a program and add them to
- * the set variables *)
-let rec collect_vars (p : Ast.program) : unit = 
-  match fst p with
-    Exp(e) -> collect_vars_exp e
-  | Seq(s1, s2) -> (collect_vars s1; collect_vars s2)
-  | If(e, s1, s2) -> (collect_vars_exp e; collect_vars s1; collect_vars s2)
-  | While(e, s) -> (collect_vars_exp e; collect_vars s)
-  | For(e1, e2, e3, s) -> (collect_vars_exp e1; collect_vars_exp e2; 
-    collect_vars_exp e3; collect_vars s)
-  | Return(e) -> collect_vars_exp e
-
-(* compiles a Fish statement down to a list of MIPS instructions.
- * Note that a "Return" is accomplished by placing the resulting
- * value in R2 and then doing a Jr R31.
- *)
-
-let zero = Word32.fromInt 0
-
-(* The only guarantee we make of an expression is that R3 is stored with its value after it is 
-   finished evaluating. It may or may not clobber R4 in the process *)
-let rec compile_exp ((e , _) : Ast.exp) : inst list =
-    match e with
-      Int j -> [Li(R3, Word32.fromInt j)]
-
-    | Var x -> [La(R3, "MOO" ^ x); Lw(R3, R3, zero)]
-
-    | Binop(e1, op, e2) -> 
-  (let t = new_temp() in 
-  (* Store return value of e1 into t *)
-  (compile_exp e1) @ [La(R4, t); Sw(R3, R4, zero)] 
-  (* Recover e1 from t into R4, meanwhile return value of e2 is in R3 *)
-  @ (compile_exp e2) @ [La(R4, t); Lw(R4, R4, zero)]
-  @ (match op with
-      Plus -> [Add(R3, R4, Reg R3)]
-    | Minus -> [Sub(R3, R4, R3)]
-    | Times -> [Mul(R3, R4, R3)]
-    | Div -> [Mips.Div(R3, R4, R3)]
-    | Eq -> [Mips.Seq(R3, R4, R3)]
-    | Neq -> [Sne(R3, R4, R3)]
-    | Lt -> [Slt(R3, R4, R3)]
-    | Lte -> [Sle(R3, R4, R3)]
-    | Gt -> [Sgt(R3, R4, R3)]
-    | Gte -> [Sge(R3, R4, R3)]
-  ))
-
-    | Not(e) -> (compile_exp e) @ [Mips.Seq(R3, R3, R0)] (* Set R3 to 1 if zero, zero otherwise *)
-
-    | And(e1, e2) -> 
-  (let t = new_temp() in 
-  let l = new_label() in
-  (* If e1 = 0 jump directly to end, otherwise store return value of e1 into t *)
-  (compile_exp e1) @ [Beq(R3, R0, l); La(R4, t); Sw(R3, R4, zero)] 
-  (* Recover e1 from t into R4, meanwhile return value of e2 is in R3 *)
-  @ (compile_exp e2) @ [La(R4, t); Lw(R4, R4, zero)]
-  (* Store if R4 is nonzero in R4, then store if R3 is nonzero in R3, then
-    bitwise and the result *)
-  @ [Label l; Sne(R4, R4, R0); Sne(R3, R3, R0); Mips.And(R3, R3, Reg(R4))])
-
-    | Or(e1, e2) -> 
-  (let t = new_temp() in 
-  let l = new_label() in
-  (* If e1 != 0 jump directly to end, otherwise store return value of e1 into t *)
-  (compile_exp e1) @ [Bne(R3, R0, l); La(R4, t); Sw(R3, R4, zero)] 
-  (* Recover e1 from t into R4, meanwhile return value of e2 is in R3 *)
-  @ (compile_exp e2) @ [La(R4, t); Lw(R4, R4, zero)]
-  (* Store if R4 is nonzero in R4, then store if R3 is nonzero in R3, then
-    bitwise or the result *)
-  @ [Label l; Sne(R4, R4, R0); Sne(R3, R3, R0); Mips.Or(R3, R3, Reg(R4))])
-
-    | Assign(x, e) -> (compile_exp e) @ [La(R4, "MOO" ^ x); Sw(R3, R4, zero)] 
-
-let rec compile_stmt ((s,_):Ast.stmt) : inst list = 
-    match s with
-    | Exp(e) -> compile_exp e
-    | Seq(s1,s2) -> (compile_stmt s1) @ (compile_stmt s2)
-    | If(e,s1,s2) ->
-        (let else_1 = new_label() in
-        let end_l = new_label() in
-        (compile_exp e) @ [Beq(R3, R0, else_1)] @
-        (compile_stmt s1) @ [J end_l; Label else_1] @
-        (compile_stmt s2) @ [Label(end_l)])
-    | While(e,s) ->
-        (let test_l = new_label() in
-        let top_l = new_label() in
-        [J test_l; Label top_l] @
-        (compile_stmt s) @
-        [Label test_l] @
-        (compile_exp e) @
-        [Bne(R3,R0,top_l)])
-  (* Rewrite For as a While loop *)
-    | For(e1,e2,e3,s) ->
-        compile_stmt((Seq((Exp e1, 0),(While(e2,(Seq(s,(Exp e3,0)),0)), 0)), 0)) 
-  (* Store the result of R3 into R2 for return *)
-    | Return(e) -> (compile_exp e) @ [Mips.Add(R2, R3, Reg(R0)); Jr(R31)]
-*)
-
 (* compiles Cish AST down to MIPS instructions; Cish has no global variables *)
 let compile (p : Ast.program) : result = 
-	let rec compile_flist (flist : Ast.func list) : Mips.inst list =
-	match p with
-	  [] -> []
-	| h::t -> (compile_func h) @ (compile_flist t) in
-	{ code = (Jal("main") :: (compile_flist p)); data = [] }
+	let code = List.fold_right (fun a b -> a @ b) (List.map compile_func p) [] in
+	{ code = (Jal("main") :: code); data = [] }
 
 let result2string (res : result) : string = 
     let code = res.code in
@@ -382,7 +268,7 @@ let result2string (res : result) : string =
 	    "\t.align\t2\n" ^
 	    "\t.globl main\n" ^
 	    (String.concat "" strs) ^
-	    "\n\n" ^
+ 		"\n\n" ^
 	    "\t.data\n" ^
 	    "\t.align 0\n"^
 	    (String.concat "" (List.map vaR8decl data)) ^
