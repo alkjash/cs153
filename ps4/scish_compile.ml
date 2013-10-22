f2259b6d00df2ccc757fe20e6d47a58f7285e6ec
Xiaoyu He <alkjash876@gmail.com>
2013-10-22 16:47:57 -0400

    Completed compiling of primops

--- ps4/scish_compile.ml
+++ ps4/scish_compile.ml
@@ -8,6 +8,7 @@ open Scish_ast
 open Cish_ast
 
 exception TODO
+exception FatalError
 
 (* Generate fresh labels *)
 let label_counter = ref 0
@@ -45,34 +46,87 @@ let lookup() =
 (* The list of helper functions is constructed as side effects of compiling the main function *)
 let flist = ref []
 
+(* Make a sequence out of a list of stmts *)
+let rec make_Seq (l : Cish_ast.stmt list) : Cish_ast.stmt =
+	match l with
+	  [] -> (skip, 0)
+	| h :: t -> (Seq(h, make_Seq(t)), 0)
+
+(* Write code to construct a pair out of two variables and store it in result *)
+let make_pair (v1 : Cish_ast.var) (v2 : Cish_ast.var) : Cish_ast.stmt =
+	(* In case either of v1, v2 is result, move these to temps first *)
+	let t1 = new_var() in
+	let t2 = new_var() in
+	let assign = make_Seq [(Exp (Assign(t1, (Cish_ast.Var v1, 0)), 0), 0); 
+				(Exp (Assign(t2, (Cish_ast.Var v2, 0)), 0), 0)] in
+	(* malloc space for the pair and store the values *)
+	make_Seq [assign; (Exp (Assign("result", (Malloc (Int(8), 0), 0)), 0), 0);
+		(Exp (Store((Cish_ast.Var "result", 0), (Cish_ast.Var t1, 0)), 0), 0);
+		(Exp (Store((Binop((Cish_ast.Var "result", 0), Plus, (Cish_ast.Int(4), 0)), 0), 
+			(Cish_ast.Var t1, 0)), 0), 0)]
+
 (* Compiles Int(i), PrimApp(p, el), If(e1, e2, e3), App(e1, e2), Lambda(v, e); 
 	calls compile_applyexp and compile_func to compile the last *)
 let rec compile_aexp (e : Scish_ast.exp) : Cish_ast.stmt = 
 	match e with
 	  Scish_ast.Int(i) -> raise TODO 
-	| PrimApp(p, el) -> match p with
-		  Plus -> 
-		| Minus ->
-		| Times ->
-		| Div ->
-		| Cons ->
-		| Fst ->
+	| PrimApp(p, el) -> (match p with
+		(* Pairs stored as pair of pointers to objects: *t is the first element, *(t+4) is second *)
+		  Fst -> 
+			(* compile el into code which puts its value in result, 
+				then store the first element of that in result *)
+			make_Seq [compile_aexp (List.hd el);
+				(Exp(Assign("result", (Load (Cish_ast.Var "result", 0), 0)), 0), 0)]
 		| Snd ->
-		| Eq ->
-		| Lt ->
+			(* compile el into code which puts its value in result, 
+				then store the second element of that in result *)
+			make_Seq [compile_aexp (List.hd el); 
+				(Exp(Assign("result", (Load
+				(Binop((Cish_ast.Var "result", 0), Plus, (Int(4), 0)), 0), 0)), 0), 0)]
+		| _ -> if List.length el <> 2 then raise FatalError else
+			let e1 = List.hd el in
+			let e2 = List.hd (List.tl el) in
+			let temp = new_var() in
+			(* Generate the last piece of code first: Assume value of e1 stored in temp,
+				value of e2 stored in result; apply p to them and store in result *)
+			let compute = match p with
+			  Scish_ast.Plus -> (Exp(Assign("result", 
+				(Binop((Cish_ast.Var "result", 0), Cish_ast.Plus, (Cish_ast.Var temp, 0)), 0)), 0), 0)
+			| Scish_ast.Minus -> (Exp(Assign("result", 
+				(Binop((Cish_ast.Var "result", 0), Cish_ast.Minus, (Cish_ast.Var temp, 0)), 0)), 0), 0)
+			| Scish_ast.Times -> (Exp(Assign("result", 
+				(Binop((Cish_ast.Var "result", 0), Cish_ast.Times, (Cish_ast.Var temp, 0)), 0)), 0), 0)
+			| Scish_ast.Div -> (Exp(Assign("result", 
+				(Binop((Cish_ast.Var "result", 0), Cish_ast.Div, (Cish_ast.Var temp, 0)), 0)), 0), 0)
+			| Scish_ast.Eq -> (Exp(Assign("result", 
+				(Binop((Cish_ast.Var "result", 0), Cish_ast.Eq, (Cish_ast.Var temp, 0)), 0)), 0), 0)
+			| Scish_ast.Lt -> (Exp(Assign("result", 
+				(Binop((Cish_ast.Var "result", 0), Cish_ast.Lt, (Cish_ast.Var temp, 0)), 0)), 0), 0)
+			| Cons -> make_pair temp "result" 
+			| _ -> raise FatalError in
+ 
+			(* Compile e1, load its result into a temp, then compile e2; all this before applying
+			   the primop *)
+			let store_temp = (Let (temp, (Cish_ast.Var "result", 0), 
+				make_Seq [compile_aexp e2; compute]), 0) in
+			make_Seq [compile_aexp e1; store_temp])
+			
 	| Scish_ast.If(e1, e2, e3) -> raise TODO 
 	| App(e1, e2) -> 
-		(* First compile e1, which has to be a lambda function, and add it into the flist *)
-		let newf = compile_func e1 (new_func()) (Some v) in
-		let _ = (flist := newf :: (flist)) in
+		((* First compile e1, which has to be a lambda function, and add it into the flist *)
+		let newf = compile_func e1 (new_func()) (Some "env") in
+		let _ = (flist := newf :: (!flist)) in
 		(* Next write code for calling e1, given that we have the function newf which takes an
 		   environment linked list *)
-		let Lambda(v, e3) = e1 in
-		compile_applyexp e2 v
-	| Lambda(v, e) ->
+		match e1 with
+		  Lambda (v, e3) -> compile_applyexp e2 v
+		| _ -> raise FatalError)
+	| Lambda(v, e1) ->
 		(* Compile the function and then compute and return a closure *)
-		let newf = compile_func e1 (new_func()) (Some v) in
+		let newf = compile_func e1 (new_func()) (Some "env") in
 		let _ = (flist := newf :: (!flist)) in
+		raise TODO
+	| _ -> raise FatalError
 
 (* Compiles an "apply" expression, writing code to evaluate an expression where a given variable is
    substituted in by the apply *)
@@ -83,8 +137,8 @@ and compile_func (e : Scish_ast.exp) (name : Cish_ast.var)
 	(arg : Scish_ast.var option) : Cish_ast.func =
 	(* First define a variable "result" which stores all the 
 	   temporary calculation values at each step, then compile the expression e into a stmt *)
-	let body = (Let "result" (Int 0, 0) (compile_aexp e), 0) in
-	let f = Fn({name = name; args = 
+	let body = (Let ("result", (Int 0, 0), (compile_aexp e)), 0) in
+	Fn({name = name; args = 
 		(match arg with
 		  None -> []
 		| Some v -> [v]); body = body; pos = 0})
