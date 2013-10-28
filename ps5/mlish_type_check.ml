1c0cd6ea10f4ef8195b4e04548e94d379d0bd234
Xiaoyu He <alkjash876@gmail.com>
2013-10-27 20:28:30 -0400

    Basic compiling complete

--- ps5/mlish_type_check.ml
+++ ps5/mlish_type_check.ml
@@ -1,4 +1,4 @@
-open Mlish_ast
+module ML = Mlish_ast
 
 exception TODO
 exception FatalError
@@ -16,37 +16,37 @@ let extend (en : env) (x : Mlish_ast.var) (ts : Mlish_ast.tipe_scheme) : env =
 let lookup (en : env) (x : Mlish_ast.var) : Mlish_ast.tipe_scheme =
 	match (List.filter (fun y -> (fst y) = x) en) with
 	  [] -> type_error ("Unbound variable" ^ x)
-	| h::t -> Snd h
+	| h::t -> snd h
 
 (***************** Type-Checking ******************************)
 
 (* Checking primitives *)
-let rec type_check_prim (r : Mlish_ast.rexp) : tipe =
+let rec type_check_prim (r : ML.rexp) : ML.tipe =
 	match r with
-	  PrimApp(p, el) -> 
+	  ML.PrimApp(p, el) -> 
 		(match p with
-		  Int _ -> Int_t
-		| Bool _ -> Bool_t
-		| Unit -> Unit_t
-		| Plus | Minus | Times | Div | Eq | Lt -> 
+		  ML.Int _ -> ML.Int_t
+		| ML.Bool _ -> ML.Bool_t
+		| ML.Unit -> ML.Unit_t
+		| ML.Plus | ML.Minus | ML.Times | ML.Div | ML.Eq | ML.Lt -> 
 			raise TODO
-		| Pair -> raise TODO
-		| Fst -> raise TODO
-		| Snd -> raise TODO
-		| Nil -> raise TODO
-		| Cons -> raise TODO
-		| IsNil -> raise TODO
-		| Hd | Tl -> raise TODO)
+		| ML.Pair -> raise TODO
+		| ML.Fst -> raise TODO
+		| ML.Snd -> raise TODO
+		| ML.Nil -> raise TODO
+		| ML.Cons -> raise TODO
+		| ML.IsNil -> raise TODO
+		| ML.Hd | ML.Tl -> raise TODO)
 	| _ -> raise FatalError
 
 (* type_check_exp returns the tipe of the given expression if it typechecks
    internally; otherwise it raises TypeError *)
-and type_check_exp (e : Mlish_ast.exp) : tipe = 
+and type_check_exp (e : ML.exp) : ML.tipe = 
 	let (r, _) = e in
 	match r with
-	  Var x -> raise TODO
-	| PrimApp (_, _) -> type_check_prim e
-	| Fn (x, e) -> raise TODO
-	| App (e1, e2) -> raise TODO
-	| If (e1, e2, e3) -> raise TODO
-	| Let (x, e1, e2) -> raise TODO
+	  ML.Var x -> raise TODO
+	| ML.PrimApp (_, _) -> type_check_prim r
+	| ML.Fn (x, e) -> raise TODO
+	| ML.App (e1, e2) -> raise TODO
+	| ML.If (e1, e2, e3) -> raise TODO
+	| ML.Let (x, e1, e2) -> raise TODO
