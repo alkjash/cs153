c4fb9a3ee7de15eaa85b1c8706660bec1769b190
Xiaoyu He <alkjash876@gmail.com>
2013-10-31 21:14:39 -0400

    Added some typechecking

--- ps5/mlish_type_check.ml
+++ ps5/mlish_type_check.ml
@@ -20,14 +20,58 @@ let lookup (en : env) (x : Mlish_ast.var) : Mlish_ast.tipe_scheme =
 
 (***************** Type-Checking ******************************)
 
-(* Checking primitives *)
-let rec type_check_prim (r : ML.rexp) : ML.tipe =
+(* Guess: creates an undetermined guess for a currently unknown type variable *)
+let guess() = 
+	ML.Guess_t (ref None)
+
+(* Substitute: Given a list subs of pairs (tvar, tipe) and an incomplete tipe (i.e. one with
+	indeterminate tvars), substitute the tipes in *)
+let rec substitute (t : ML.tipe) (subs : (ML.tvar * ML.tipe) list) : tipe =
+	(* Helper: find the value of tv in subs *)
+	let find_tipe tv =
+		snd (List.hd (List.filter (fun p -> ((fst p) = tv)) subs)) in
+	match t with
+	  ML.Tvar_t tv -> find_tipe tv
+	| ML.Int_t -> ML.Int_t
+	| ML.Bool_t -> ML.Bool_t
+	| ML.Unit_t -> ML.Unit_t
+	| ML.Fn_t (x, y) -> ML.Fn_t (substitute x subs, substitute y subs)
+	| ML.Pair_t (x, y) -> ML.Pair_t (substitute x subs, substitute y subs)
+	| ML.List_t t2 -> ML.List_t (substitute t2 subs)
+	| ML.Guess_t (ref None) -> ML.Guess_t (ref None)
+	| ML.Guess_t (ref Some t2) -> ML.Guess_t (ref Some (substitute t2 subs))
+
+(* Instantiate: substitute guesses for all the unknown variables *)
+let instantiate (ts : ML.tipe_scheme) : tipe =
+	let Forall(vs, t) = ts in
+	let vs_and_ts = map (fun a -> (a, guess()) vs in
+	substitute t vs_and_ts
+
+(* Unify: check if t1 and t2 can be of the same tipe; improve guesses so that
+   they are the same tipe at the end *)
+let rec unify (t1 : ML.tipe) (t2 : ML.tipe) : bool =
+	match (t1, t2) with
+	  (t, t) -> true
+	| (ML.Guess_t (ref(Some t3)), _) -> unify t3 t2
+	| (ML.Guess_t (r as (ref None)), _) -> (r := t2; true)
+	| (_, ML.Guess_t(_)) -> unify t2 t1
+	| (ML.Fn_t(a1, b1), ML.Fn_t(a2, b2)) 
+		-> (unify a1 a2) && (unify b1 b2)
+	| ML.Pair_t(a1, b1), ML.Pair_t(a2, b2) 
+		-> (unify a1 a2) && (unify b1 b2)
+	| _ -> false
+
+(* Checking primitive MLish expressions *)
+let rec type_check_prim (en : env) (r : ML.rexp) : ML.tipe =
 	match r with
 	  ML.PrimApp(p, el) -> 
 		(match p with
-		  ML.Int _ -> ML.Int_t
-		| ML.Bool _ -> ML.Bool_t
+		  ML.Int _ -> 
+			if List.length el = 0 then ML.Int_t else type_error()
+		| ML.Bool _ -> 
+			if List.length el = 0 then ML.Bool_t else type_error()
 		| ML.Unit -> ML.Unit_t
+			if List.length el = 0 then ML.Unit_t else type_error()
 		| ML.Plus | ML.Minus | ML.Times | ML.Div | ML.Eq | ML.Lt -> 
 			raise TODO
 		| ML.Pair -> raise TODO
@@ -41,12 +85,23 @@ let rec type_check_prim (r : ML.rexp) : ML.tipe =
 
 (* type_check_exp returns the tipe of the given expression if it typechecks
    internally; otherwise it raises TypeError *)
-and type_check_exp (e : ML.exp) : ML.tipe = 
+and type_check_exp (en : env) (e : ML.exp) : ML.tipe = 
 	let (r, _) = e in
 	match r with
-	  ML.Var x -> raise TODO
-	| ML.PrimApp (_, _) -> type_check_prim r
-	| ML.Fn (x, e) -> raise TODO
-	| ML.App (e1, e2) -> raise TODO
-	| ML.If (e1, e2, e3) -> raise TODO
+	  ML.Var x -> 
+		instantiate (lookup en x)
+	| ML.PrimApp (_, _) -> 
+		type_check_prim r
+	| ML.Fn (x, e) -> 
+		let g = guess() in
+		ML.Fn_t (g, type_check_exp (extend en x Forall([], g)) e)
+	| ML.App (e1, e2) -> 
+		let (t1, t2, t) = (type_check_exp en e1, type_check_exp en e2, guess()) in
+		if unify t1 Fn_t(t2, t) then t else type_error()
+	| ML.If (e1, e2, e3) -> 
+		if unify (type_check_exp en e1) Bool_t then
+			let (t2, t3) = (type_check_exp en e2, type_check_exp en e3) in
+			if unify t2 t3 then t2 else type_error()
+		else 
+			type_error()
 	| ML.Let (x, e1, e2) -> raise TODO
